5efa9e69f5bce170af935c843033d6dc778eceb0
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-12-06 00:45:47 +0000

    Refactor personal projects cards

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -13,23 +13,14 @@ interface Props {
 
 export function ProjectCard({ title, description, tags, link, medium, preview }: Props) {
 	return (
-		<Card className="relative flex flex-col overflow-hidden">
-			<div className="flex relative bg-muted rounded-lg rounded-bl-none rounded-br-none h-32 w-full overflow-hidden">
-				{/* // TODO: Review grad for per project spec. */}
-				{/* <div
-					className="absolute h-[600px]  lg:h-[1100px] w-full right-0 top-0
-		  bg-[radial-gradient(ellipse_at_center_200px,_var(--tw-gradient-stops))]
-		  from-green-400/40 via-white/70 to-white to-[67%]
-		  dark:from-green-400/40 dark:via-black/70 dark:to-black dark:to-[67%]"
-				/> */}
-
-				{/* // TODO: Need a fallback here. */}
+		<Card className="group relative flex flex-col h-48 overflow-hidden">
+			<div className="flex relative bg-muted rounded-lg rounded-bl-none rounded-br-none h-full w-full overflow-hidden">
 				{preview && (
 					<Image
-						className="self-center md:blur-[2px] hover:blur-none transition-all duration-700 ease-in-out"
+						className="self-start group-hover:scale-105 ease-in-out duration-1000"
 						src={preview}
 						alt={`Image preview of project ${title}`}
-						height={200}
+						height={400}
 						width={800}
 					/>
 				)}
@@ -43,39 +34,47 @@ export function ProjectCard({ title, description, tags, link, medium, preview }:
 					{medium}
 				</Badge>
 			</div>
-			<CardHeader>
-				<div className="space-y-1">
-					<CardTitle className="text-base">
-						{link ? (
-							<a
-								href={link}
-								target="_blank"
-								rel="noreferrer"
-								className="inline-flex items-center gap-1 hover:underline"
-							>
-								{title} <span className="h-1 w-1 rounded-full bg-green-500"></span>
-							</a>
-						) : (
-							title
-						)}
-					</CardTitle>
-					<div className="hidden font-mono text-xs underline print:visible">
-						{link?.replace('https://', '').replace('www.', '').replace('/', '')}
+
+			<div className="absolute bottom-0 w-full translate-y-44 group-hover:translate-y-0 border-t duration-700 bg-card/95">
+				<CardHeader>
+					<div className="space-y-1">
+						<CardTitle className="text-base">
+							{link ? (
+								<a
+									href={link}
+									target="_blank"
+									rel="noreferrer"
+									className="inline-flex items-center gap-1 hover:underline"
+								>
+									{title} <span className="h-1 w-1 rounded-full bg-green-500" />
+								</a>
+							) : (
+								title
+							)}
+						</CardTitle>
+						<div className="hidden font-mono text-xs underline print:visible">
+							{link?.replace('https://', '').replace('www.', '').replace('/', '')}
+						</div>
+						<CardDescription className="font-mono text-xs text-foreground">
+							{description}
+						</CardDescription>
 					</div>
-					<CardDescription className="font-mono text-xs text-foreground">
-						{description}
-					</CardDescription>
-				</div>
-			</CardHeader>
-			<CardContent className="mt-auto flex">
-				<div className="mt-2 flex flex-wrap gap-1">
-					{tags.map((tag) => (
-						<Badge className="px-2 py-1 text-[10px]" variant="outline" key={tag}>
-							{tag}
-						</Badge>
-					))}
-				</div>
-			</CardContent>
+				</CardHeader>
+
+				<CardContent className="mt-auto flex">
+					<div className="mt-2 flex flex-wrap gap-1">
+						{tags.map((tag) => (
+							<Badge
+								className="px-2 py-1 text-[10px]"
+								variant="outline"
+								key={`${title}_${tag}`}
+							>
+								{tag}
+							</Badge>
+						))}
+					</div>
+				</CardContent>
+			</div>
 		</Card>
 	);
 }
