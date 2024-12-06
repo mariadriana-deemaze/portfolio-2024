f42b5e52ee2939cc2084c641418fc32e3121979c
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-12-06 17:02:13 +0000

    Apply on touch events

--- components/ui/section-reveal.tsx
+++ components/ui/section-reveal.tsx
@@ -5,30 +5,42 @@ const ScrollFadeReveal = ({ children }: { children: ReactNode }) => {
 	const ref = useRef<HTMLDivElement>(null);
 
 	// @ts-expect-error
-	useLenis(({ scroll, isScrolling, actualScroll, velocity, direction }) => {
+	useLenis(({ isScrolling, velocity }) => {
 		if (ref.current) {
 			const elementTop = ref.current.offsetTop;
 			const elementBottom = elementTop + ref.current.offsetHeight;
 			const windowTop = window.scrollY;
 			const windowBottom = windowTop + window.innerHeight;
 
-			if (windowBottom > elementTop && windowTop < elementBottom) {
-				ref.current.classList.add('scroll-fade-in');
+			const handleScroll = () => {
+				if (ref.current) {
+					if (windowBottom > elementTop && windowTop < elementBottom) {
+						ref.current.classList.add('scroll-fade-in');
 
-				if (isScrolling) {
-					let skewX = 0;
-					let skewY = Math.max(-5, Math.min(5, velocity * 2));
+						if (isScrolling) {
+							let skewX = 0;
+							let skewY = Math.max(-5, Math.min(5, velocity * 2));
 
-					console.log('skew ->', skewX, skewY);
+							console.log('skew ->', skewX, skewY);
 
-					ref.current.style.transform = `translateY(0) skewX(0deg) skewY(${skewY}deg)`;
-				} else {
-					ref.current.style.transform = `translateY(0) skewX(0deg) skewY(0deg)`;
+							ref.current.style.transform = `translateY(0) skewX(0deg) skewY(${skewY}deg)`;
+						} else {
+							ref.current.style.transform = `translateY(0) skewX(0deg) skewY(0deg)`;
+						}
+					} else {
+						ref.current.classList.remove('scroll-fade-in');
+						ref.current.style.transform = `translateY(0) skewX(0deg) skewY(0deg)`;
+					}
 				}
-			} else {
-				ref.current.classList.remove('scroll-fade-in')
-				ref.current.style.transform = `translateY(0) skewX(0deg) skewY(0deg)`;
-			}
+			};
+
+			window.addEventListener('scroll', handleScroll);
+			window.addEventListener('touchmove', handleScroll);
+
+			return () => {
+				window.removeEventListener('scroll', handleScroll);
+				window.removeEventListener('touchmove', handleScroll);
+			};
 		}
 	});
 
