05453527d4cc895ff1c02438c70fbf6986fb9f41
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-12-06 16:49:29 +0000

    Scroll animations with Lenis

--- components/ui/section-reveal.tsx
+++ components/ui/section-reveal.tsx
@@ -1,10 +1,11 @@
-import React, { ReactNode, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
 import { useLenis } from '@studio-freight/react-lenis';
 
 const ScrollFadeReveal = ({ children }: { children: ReactNode }) => {
 	const ref = useRef<HTMLDivElement>(null);
 
-	useLenis(({ scroll, isScrolling }) => {
+	// @ts-expect-error
+	useLenis(({ scroll, isScrolling, actualScroll, velocity, direction }) => {
 		if (ref.current) {
 			const elementTop = ref.current.offsetTop;
 			const elementBottom = elementTop + ref.current.offsetHeight;
@@ -13,15 +14,21 @@ const ScrollFadeReveal = ({ children }: { children: ReactNode }) => {
 
 			if (windowBottom > elementTop && windowTop < elementBottom) {
 				ref.current.classList.add('scroll-fade-in');
-			}
 
-			if (isScrolling) {
-				ref.current.classList.add('scroll-skew');
+				if (isScrolling) {
+					let skewX = 0;
+					let skewY = Math.max(-5, Math.min(5, velocity * 2));
+
+					console.log('skew ->', skewX, skewY);
+
+					ref.current.style.transform = `translateY(0) skewX(0deg) skewY(${skewY}deg)`;
+				} else {
+					ref.current.style.transform = `translateY(0) skewX(0deg) skewY(0deg)`;
+				}
 			} else {
-				ref.current.classList.remove('scroll-skew');
+				ref.current.classList.remove('scroll-fade-in')
+				ref.current.style.transform = `translateY(0) skewX(0deg) skewY(0deg)`;
 			}
-
-			//.setProperty('--scroll-skew-percentage', "10deg")
 		}
 	});
 
