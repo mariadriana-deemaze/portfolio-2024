bb9f760cc6fb4cdfd8a2e9854c338c6134c34f0c
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2025-03-31 21:29:20 +0100

    Paragraph style correction

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,9 +14,9 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
 	return (
-		<div className="mx-auto text-center max-w-96 mt-10 flex flex-col items-center justify-center">
+		<div className="mx-auto text-center max-w-[450px] mt-10 flex flex-col items-center justify-center">
 			<h2 className="text-fade-grad font-clash text-4xl font-semibold">Not found</h2>
-			<p className="mt-4">
+			<p className="mt-4 text-pretty font-mono text-sm text-foreground">
 				Whatever you were looking for, is simply not here. <br />
 				<i>Have you tried looking under the bed?</i>
 			</p>
