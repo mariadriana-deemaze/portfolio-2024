f142507a44570229c12deebe7fca8d0feb72330e
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2025-11-06 22:03:28 +0000

    Migrate Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Work from './pages/Work';
 import About from './pages/About';
 import Blog from './pages/Blog';
+import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
 
@@ -28,6 +29,7 @@ export default function App(props: AppProps = {}): JSX.Element {
         <Route path="/work" element={<Work />} />
         <Route path="/about" element={<About message={props.initialData?.message} />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,34 @@
+import { JSX } from 'react'
+import { Section } from '@/components/ui/section'
+import { ContactForm } from '@/components/pages/contact/form'
+
+export default function Contact(): JSX.Element {
+  return (
+    <>
+      <header className="mx-auto w-full max-w-2xl space-y-2 animate-fade-in-left delay-500 mb-4">
+        <h1 className="font-clash font-bold text-5xl text-fade-grad">Contact</h1>
+        <h4 className="font-clash font-medium text-md text-gray-500">Reach out</h4>
+        <p className="pb-10 text-pretty font-mono text-sm text-foreground leading-5">
+          I'm always interested in new projects and opportunities. Feel free to get in
+          touch with me if you have a project in mind, want to discuss a potential
+          collaboration, or simply want to say hello. You can reach me through the contact
+          form below.
+          <br />
+          Typically I respond back to emails within{' '}
+          <span className="text-orange-600">1 business day</span>.
+        </p>
+        <hr />
+      </header>
+
+      <Section className="animate-fade-in-left delay-700">
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col items-center align-middle self-center max-w-2xl w-full gap-10">
+            <div className="flex flex-col w-full gap-4"></div>
+            <ContactForm />
+          </div>
+        </div>
+      </Section>
+    </>
+  )
+}
+
