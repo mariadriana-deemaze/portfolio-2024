5c2381311b5ecf0b5315c5b41a1e3b653c12c5be
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2026-01-15 19:51:22 +0000

    chore 🧰 : eslint config to enforce consistent casing and file ordering

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import perfectionist from 'eslint-plugin-perfectionist'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import unicorn from 'eslint-plugin-unicorn'
+import globals from 'globals'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'dist-ssr'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -17,16 +21,32 @@ export default [
       },
     },
     plugins: {
+      perfectionist,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      unicorn,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+          varsIgnorePattern: '^[A-Z_]',
+        },
+      ],
+      'perfectionist/sort-imports': ['error', { type: 'natural' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+        },
       ],
     },
   },
