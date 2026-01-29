950b66fd0d7815cf460275174ed794fc51caa754
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2026-01-29 21:47:12 +0000

    fix 🐛 : hydration issue correction

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import App, { type AppProps } from '@/app';
 import { initThemeFromStorage } from '@/utils/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { hydrateRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
 declare global {
   interface Window {
@@ -15,10 +15,14 @@ const initialProps: AppProps | undefined = window.__INITIAL_PROPS__;
 if (rootElement) {
   initThemeFromStorage();
   const queryClient = new QueryClient();
-  hydrateRoot(
-    rootElement,
+  const app = (
     <QueryClientProvider client={queryClient}>
       <App {...(initialProps ?? {})} />
     </QueryClientProvider>
   );
+  if (initialProps && rootElement.hasChildNodes()) {
+    hydrateRoot(rootElement, app);
+  } else {
+    createRoot(rootElement).render(app);
+  }
 }
