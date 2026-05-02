a9bf3a234517a91ed44287240e73ecf6f6d28164
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2026-05-02 23:10:38 +0100

    deps 📦 : remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tanstackStart(), nitro(), react(), tsconfigPaths()]
+	resolve: {
+		tsconfigPaths: true
+	},
+	plugins: [tanstackStart(), nitro(), react()]
 });
